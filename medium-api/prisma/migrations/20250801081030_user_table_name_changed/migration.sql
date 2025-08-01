e78275133f91e5d8e8315093b578371b73849de6
heronrush <isonade.git@gmail.com>
2025-08-01 15:30:20 +0530

    api is functional a bit

---
+++ medium-api/prisma/migrations/20250801081030_user_table_name_changed/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropTable
+DROP TABLE "public"."User";
+
+-- CreateTable
+CREATE TABLE "public"."Users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "public"."Users"("email");
+
+-- AddForeignKey
+ALTER TABLE "public"."Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "public"."Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- medium-api/src/index.ts
+++ medium-api/src/index.ts
@@ -1,62 +1,88 @@
-import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { sign } from "hono/jwt";
-import { PrismaClient } from "./generated/prisma/edge";
+import { Hono } from "hono";
+import { contextStorage } from "hono/context-storage";
+import { use } from "hono/jsx";
+import { decode, jwt, sign, verify } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
-    DATABASE_URL: string;
+    CONNECTION_POOL_URL: string;
+    JWT_SECRET: string;
   };
 }>();
 
-app.get("/", (c) => {
-  return c.text("Hello Honosjoai eo!");
-});
-
-// signup route
-app.post("/api/v1/signup", async (c) => {
+// signup endpoint
+app.post("/api/v1/user/signup", async (c) => {
   const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
+    datasourceUrl: c.env.CONNECTION_POOL_URL,
   }).$extends(withAccelerate());
 
-  // extract the body
-  const reqBody = await c.req.json();
+  const body = await c.req.json();
 
-  const user = await prisma.user.create({
-    data: {
-      email: reqBody.email,
-      password: reqBody.password,
-    },
-  });
+  try {
+    const user = await prisma.users.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
 
-  const token = await sign({ id: user.id }, "secret");
+    const token = await sign({ id: user.id }, c.env.JWT_SECRET);
 
-  return c.json({ jwt: token });
+    return c.json({ token: token });
+  } catch (e) {
+    return c.json({ msg: e });
+  }
 });
 
-// signin route
-app.post("/api/v1/signin", (c) => {
-  return c.text("api/v1/signin user signin route");
+// signin endpoint
+app.post("/api/v1/user/signin", async (c) => {
+  const body = await c.req.json();
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.CONNECTION_POOL_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const user = await prisma.users.findFirst({
+      where: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    if (!user) {
+      return c.json({ msg: "incorrect credentials" });
+    }
+
+    const jwt = await sign(
+      {
+        id: user.id,
+      },
+      c.env.JWT_SECRET
+    );
+
+    return c.json({ token: jwt });
+  } catch (e) {
+    return c.text("error - signin");
+  }
 });
 
-// blog posting route
 app.post("/api/v1/blog", (c) => {
-  return c.text("api/v1/blog new blog posting route");
+  return c.text("Hello Hono!");
 });
 
-// blog updating route
 app.put("/api/v1/blog", (c) => {
-  return c.text("api/v1/signup blogs update route");
+  return c.text("Hello Hono!");
 });
 
-// getting all blogs route
 app.get("/api/v1/blog/:id", (c) => {
-  return c.text("getting all blogs route");
+  return c.text("Hello Hono!");
 });
 
-// not found route
-app.notFound((c) => {
-  return c.text("custom 404 not found message", 404);
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text("Hello Hono!");
 });
 
 export default app;
